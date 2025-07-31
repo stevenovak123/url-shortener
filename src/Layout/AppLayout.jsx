20f1dd2b0c21f6b9a1c1a8f724adb8b59e703928
stevenovak123 <stevenovak0802@gmail.com>
2025-07-31 18:50:38 +0530

    fixed layout and using containers

--- src/Layout/AppLayout.jsx
+++ src/Layout/AppLayout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom';
 const AppLayout = () => {
   return (
     <div>
-      <main className='min-h-screen mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+      <main className='min-h-screen mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 container'>
         <Header />
         <Outlet />
       </main>
